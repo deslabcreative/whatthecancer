9ba4ea3a294a7523dbe6eaa2748abb9a9f306257
Jayden Szekely <jayden@deslab.co.nz>
2018-10-15 15:51:03 +1300

    Update src/components/Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,12 +4,24 @@ import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
 
 const Navbar = () => (
-  <nav className="navbar">
-    <Link to="/" className="navbar-item">
+  <nav className="navbar is-transparent">
+    <div className="container">
+      <div className="navbar-brand">
+        <Link to="/" className="navbar-item">
           <figure className="image">
-            <img src={logo} alt="What The Cancer" style={{ width: '10vw', marginTop: '5vh', marginLeft: '5%'}} />
+            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
           </figure>
-    </Link>
+        </Link>
+      </div>
+      <div className="navbar-start">
+        <Link className="navbar-item" to="/about">
+          About
+        </Link>
+        <Link className="navbar-item" to="/products">
+          Blog
+        </Link>
+      </div>
+    </div>
   </nav>
 )
 
