b79afec104ee9fc6aad07555b00bd842524e4b98
Jayden Szekely <jayden@deslab.co.nz>
2018-10-17 10:36:55 +1300

    Resources Set Up

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,46 +12,41 @@ const Home = ({children}) => (
         </header>
       </section>
     </Layout>
-    <section className="article">
+    <section className="article greenLink">
     <p>
-          Hi I’m Lisa Dawson. I am a Medical Oncologist, Lifestyle Medicine champion, published author
-          and mother of 6 children.  I’m bringing you this podcast and website to help you discern what
-          to believe on social media and the internet about your cancer journey. I hope to help you 
-          take control of your cancer journey and discover how much you can do to make that journey better
-          and more successful.  I initially started out in my medicine career as a GP 
-          and saw the huge need for patients to be equipped with life changing truths about the role of diet, 
-          exercise, stress and sleep to be healthy. 80% of chronic disease today is preventable by a change in
-          our lifestyles. To that end I wrote a book called Healthy Happy Free in 2005 encouraging people to
-          change their lifestyles to prevent and reverse ill health.
-          <br /><br />
-          In 2008 I changed track in my career and re-trained as a Medical Oncologist. I was initially
-          skeptical about chemotherapy. I have always had a somewhat questioning but open mind. After many years 
-          of treating cancer patients I am left in no doubt that chemotherapy can make a big impact on cancer, 
-          sometimes curing people sometimes improving quality of life as cancer is brought under control and 
-          symptoms subside.  In those we can’t cure we can certainly buy some quality time for many. In the 
-          last few years the advent of some remarkable new drugs has seen a significant improvement in cure 
-          rates and how long people can live even if not cured. In particular immunotherapy in showing huge 
-          promise in extending life and even curing late stage 4 cancers. It’s been exciting to be part of this. 
-          Despite the sometimes questionable ethics of drug companies the drugs themselves show huge promise 
-          when used in the right way.  Despite this I think the medical profession as a whole  has been slow to 
-          pick up on the advances being made in the area of ‘lifestyle medicine’. By attending to diet, nutrition, 
-          stress and sleep we can make a substantial difference to someone staying cancer free after curative 
-          treatment, to chemotherapy side effects and someone’s quality and length of life if they have stage 4 
-          (incurable) cancer. If we had drugs that offered the benefits exercise does for example we wouldn’t 
-          hesitate to prescribe it for everybody but it’s hardly mentioned as part of a person’s treatment or 
-          recovery plan. I think this is because we are quite precise as doctors and we need to be able to offer 
-          well evidenced advice in a concise way, The reality is as doctors we don’t have clear cut ‘lifestyle 
-          prescriptions’ we can offer patients and there is still a lot of debate over things like ‘the best diet’!  
-          Doctors also have little time to address these things and are a little skeptical as patients rarely 
-          succeed in following this sort of advice when given generally.  
-          <br /><br />
-          Recently I have joined the faculty of a 
-          company called Prekure. The faculty have internationally respected leaders in ‘lifestyle medicine’ 
-          helping design and write courses for both health professionals and patients. I’m excited as we start 
-          to see the tipping point reached in medicine where we will soon see doctors and nurses equipped to 
-          prescribe lifestyle changes to help patients get well and stay well.  These course and prescriptions 
-          are under construction but will soon be available from this website. In addition to this we hope to 
-          raise up a ‘new profession’ of health coaches to help patients make the necessary changes.
+    Hi I’m Lisa Dawson, I’m a medical Oncologist and lifestyle medicine advocate.
+    <br /><br />
+    If you have been diagnosed with cancer, had cancer or are frightened of getting cancer you may have headed
+    to the internet to get some good advice and been bombarded with mixed messages; hype, conspiracy theory, fake
+    news and slick truth that isn’t truth at all. You feel more confused than ever and have no idea to trust.
+    This drives me crazy as a cancer doctor, a mum and a patient advocate. Just when you need someone to guide
+    you on this journey called cancer you feel more confused than ever.<br /><br />
+    I’ve been a cancer doctor for 10 years. Prior to that I was a family doctor for 10 years. During that time
+    I had 6 children one with a life threatening heart condition we continue to live with. I know what it is like
+    to be busy and I know what it is like to be scared and wanting to do your absolute best to survive. <br />
+    As a GP I became a passionate advocate of lifestyle medicine and wrote had a book on holistic health published
+    in 2005. I had the privilege of speaking to people on the topic of wellness and health, at that time relating
+    to depression in particular. My study at the time led me to believe in the irrefutable truth that lifestyle is
+    medicine. Since I have become an Oncologist I have discovered this is just as true for cancer as other chronic
+    medical conditions. There are things we can do with diet, exercise, self care and sleep that can dramatically
+    reduce our risk of cancer and if you already have incurable cancer help you live well for longer (or even be
+    cured).<br /><br />
+    During my time treating cancer patients I have come to deeply respect my profession. The drugs we use in cancer
+    treatment can work and can cure. Newer drugs come with less side effects, are more effective and we have better
+    drugs for managing side effects. Drug companies have made it possible to invest large amounts of money in to
+    research and development. There have been questionable ethics around the behaviour of drugs companies, especially
+    around sales and marketing but the drugs themselves used as intended are an important weapon in the battle
+    against cancer.. Combining our knowledge of drugs and lifestyle therapy it is possible to get the best treatment
+    and prevention plan.<br /><br />
+    Over the years I have helped many of my patients make choices right for them. I want to make what I have leaned
+    available for everybody. I have created a guide on <Link to="/what-you-can-do">'What <b>YOU</b> can do’ </Link>
+    to help yourself which you can <Link to="/what-you-can-do">download for free.</Link> This is the same guide I give to my patients. In upcoming blogs and podcasts I will explain
+    the information in that booklet to you in more detail. I have some great people lined up to interview on my
+    podcasts, some inspiring patients stories, great advice on implementing the suggestions and some video series
+    helping you understand the  basics of cancer.<br /><br />
+    Avoid 'drowning', instead take control knowing you will have the best and latest research at your fingertips.
+    It’s been a privilege to journey with my patients and I look forward to sharing the same journey with you.
+
 
           </p>
     <br></br>
