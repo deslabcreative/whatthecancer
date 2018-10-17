922e6550809a0bb3d32bf20fae77bbc7681149ce
Jayden Szekely <jayden@deslab.co.nz>
2018-10-17 13:57:23 +1300

    trying something

--- src/templates/home-page.js
+++ src/templates/home-page.js
@@ -3,7 +3,17 @@ import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
 import Layout from '../components/Layout'
 
-const Home = ({children}) => (
+export const HomePageTemplate = ({
+  image,
+  title,
+  heading,
+  description,
+  intro,
+  main,
+  testimonials,
+  fullImage,
+  pricing,
+}) => (
   <div>
     <Layout>
       <section>
@@ -63,6 +73,73 @@ const Home = ({children}) => (
   </div>
   
   
-)
+  )
 
-export default Home
+  HomePageTemplate.propTypes = {
+    image: PropTypes.string,
+    title: PropTypes.string,
+    heading: PropTypes.string,
+    description: PropTypes.string,
+    intro: PropTypes.shape({
+      blurbs: PropTypes.array,
+    }),
+    main: PropTypes.shape({
+      heading: PropTypes.string,
+      description: PropTypes.string,
+      image1: PropTypes.object,
+      image2: PropTypes.object,
+      image3: PropTypes.object,
+    }),
+    testimonials: PropTypes.array,
+    fullImage: PropTypes.string,
+    pricing: PropTypes.shape({
+      heading: PropTypes.string,
+      description: PropTypes.string,
+      plans: PropTypes.array,
+    }),
+  }
+  
+  const HomePage = ({ data }) => {
+    const { frontmatter } = data.markdownRemark
+  
+    return (
+      <Layout>
+        <HomePageTemplate
+          image={frontmatter.image}
+          title={frontmatter.title}
+          heading={frontmatter.heading}
+          description={frontmatter.description}
+          intro={frontmatter.intro}
+          main={frontmatter.main}
+          testimonials={frontmatter.testimonials}
+          fullImage={frontmatter.full_image}
+          pricing={frontmatter.pricing}
+        />
+      </Layout>
+    )
+  }
+  
+  HomePage.propTypes = {
+    data: PropTypes.shape({
+      markdownRemark: PropTypes.shape({
+        frontmatter: PropTypes.object,
+      }),
+    }),
+  }
+  
+  export default HomePage
+  
+  export const HomePageQuery = graphql`
+    query HomePage($id: String!) {
+      markdownRemark(id: { eq: $id }) {
+        frontmatter {
+          title
+          image
+          heading
+          description
+          
+        }
+      }
+    }
+  `
+  
