a6fd0747d5e2266d942eca4dd7ea05572ea99589
Jayden Szekely <jayden@deslab.co.nz>
2018-10-26 16:00:30 +1300

    Update

--- src/pages/listen/index.js
+++ src/pages/listen/index.js
@@ -17,12 +17,11 @@ const Home = ({children}) => (
     Here at What The Cancer, we believe in helping you understand the sensitive topic of cancer in the sea of misinformation on the internet.  Not only do we provide you with our website with Blog Posts and more, we also have all our Videos and Podcasts on all major services which are listed below to make our content accessible to you and your lifestyle.
     <br></br>
     <br></br>
-    <a href="https://itunes.apple.com/us/podcast/what-the-cancer/id1438188471?"></a>
-    <a href="https://open.spotify.com/show/00H6GVQYUsn5NtyFREO9Vs?si=dCoDFUxOROCcdHtRlu6eWA"></a>
+    <a href="https://itunes.apple.com/us/podcast/what-the-cancer/id1438188471?">Listen on Apple Podcasts</a><br></br>
     <iframe src="https://open.spotify.com/embed/show/00H6GVQYUsn5NtyFREO9Vs" width="100%" height="300" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+    <iframe src="https://widget.spreaker.com/player?show_id=3196045&theme=dark&playlist=show&playlist-continuous=false&autoplay=false&live-autoplay=false&chapters-image=true&episode_image_position=left&hide-logo=false&hide-likes=false&hide-comments=false&hide-sharing=false" width="100%" height="350px" frameborder="0"></iframe>
 
-
-          </p>
+    </p>
     <br></br>
     <br></br>
     </section>
