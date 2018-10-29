d41617031a7887cba81579d67a7f3f9669c422a9
Jayden Szekely <jayden@deslab.co.nz>
2018-10-30 09:10:48 +1300

    Some Gatsby Stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,13 @@ module.exports = {
         name: 'images',
       },
     },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/img`,
+        name: 'images',
+      },
+    },
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     {
