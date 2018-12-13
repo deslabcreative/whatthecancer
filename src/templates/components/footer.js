31a5c5d5ec40325aff9c5b3f89dc243a7f2cab28
Jayden Szekely <jayden@deslab.co.nz>
2018-12-13 15:34:01 +1300

    footer update

--- src/templates/components/footer.js
+++ src/templates/components/footer.js
@@ -7,16 +7,15 @@ const Footer = ({children, background, recommendedTitle, authorName, authorBio})
             <p>
                 <a href="https://www.whatthecancer.com" title="What The Cancer">Share whatthecancer.com</a><br></br>
                 <a href="https://www.whatthecancer.com" title="Home - What The Cancer">Home</a><br></br>
-                <a href="https://www.whatthecancer.com/about" title="About - What The Cancer">About</a><br></br>
-                <a href="https://www.whatthecancer.com/lisa" title="Dr Lisa Dawson - What The Cancer">About Lisa</a><br></br>
-                <a href="https://www.whatthecancer.com/contact" title="Ask a Question - What The Cancer">Ask a question</a><br></br>
+                <a href="https://www.whatthecancer.com/about" title="About - What The Cancer">About Us</a><br></br>
+                <a href="https://www.whatthecancer.com/lisa" title="Dr Lisa Dawson - What The Cancer">Dr Lisa Dawson</a><br></br>
                 <a href="https://www.whatthecancer.com/resources" title="Resources - What The Cancer">Our Resources</a><br></br>
-                <a href="https://www.whatthecancer.com/listen" title="Listen to our Podcast - What The Cancer">Listen</a><br></br>
+                <a href="https://www.whatthecancer.com/listen" title="Listen to our Podcast - What The Cancer">Listen to our Podcast</a><br></br>
                 <a href="https://www.whatthecancer.com/jargon" title="Understand what the Medical Jargon surrounding your cancer journey means in plain English - What The Cancer">Jargon Dictionary</a><br></br>
                 <br></br>
                 <br></br>
                 <span className="left-fifty">© What The Cancer, 2018</span>
-                <img className="deslab-logo" src="../../../img/deslab-blue.png" alt="Deslab Creative" />
+                <a href="http://www.deslab.co.nz" title="A Deslab Creative Website"><img className="deslab-logo" src="../../../img/deslab-blue.png" alt="Deslab Creative" /></a>
             </p>
         </div>
         
